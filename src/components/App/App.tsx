6d13da9ba02bb785fc919114d9360c49739cc4f4
Ezequiel Freire <ezequiel.a.freire@gmail.com>
2021-12-24 16:35:05 -0300

    component app

---
+++ src/components/App/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Aca va el form
+      </header>
+    </div>
+  );
+}
+
+export default App;
